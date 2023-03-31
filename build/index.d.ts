beeee84555271853593727aa0da83ce66a0cefeb
TONE-LYLE <50530488@qq.com>
2023-03-31 23:51:01 +0800

    git拦截提交测试

--- build/index.d.ts
+++
@@ -1,3 +0,0 @@
-declare const test: () => void;
-
-export { test };
--- build/index.js
+++
@@ -1,8 +0,0 @@
-// src/index.ts
-var test = () => {
-  console.log("\u6846\u67B6\u521D\u59CB\u5316\u6D4B\u8BD5");
-};
-export {
-  test
-};
-//# sourceMappingURL=index.js.map
